b4b35a2d0d661fe189fd523134c2a5bd635400a1
segmentationfaulter <segmentationfaulter@gmail.com>
2015-11-16 21:48:53 +0500

    implement function to extract news ids in an array

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict';
+const fetchHTML = require('./lib/fetch_html');
+const getNewsURIs = require('./lib/extract_news_uris');
+const getNewsIDs = require('./lib/extract_news_ids');
+const url = require('./lib/config').url;
+
+fetchHTML(url, (err, html) => {
+  if (err) throw err;
+  var ids = getNewsIDs(getNewsURIs(html));
+  console.log(ids);
+});
