fcd6a504002449766a022a28878ce730fe2a6670
segmentationfaulter <segmentationfaulter@gmail.com>
2015-11-18 08:11:20 +0500

    Finally! tweet the updated news!

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 const poll = require('./lib/poll');
+const getTweetContents = require('./lib/create_tweet_contents');
+const tweet = require('./lib/tweet');
 
 poll((err, updates) => {
   if (err) throw err;
-  if (updates.length === 0) {
-    console.log('No updates');
-  } else {
-    console.log(updates);
+  if (updates.length !== 0) {
+    getTweetContents(updates, (err, tweets) => {
+      tweet(tweets);
+      console.log('tweeted');
+    });
   }
 });
